34dc95275d489d846f565601d37dcbe605c0da25
Emerson Rocha Luiz <emerson@alligo.com.br>
2016-07-09 20:25:35 -0300

    Refractoring of the nodeapp

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ Quickstart:
 
     docker build -t alligo/reverse-proxy-cdn .
     docker run --rm -p 80:80 -p 443:443 -p 8888:8888 -p 8080:8080 --name mycdn alligo/reverse-proxy-cdn
+    docker run --rm -p 80:80 -p 443:443 -p 8888:8888 -p 8080:8080 --ulimit memlock=120000:120000 --name mycdn alligo/reverse-proxy-cdn
     docker exec -it mycdn bash
-
+    docker run -it --rm --ulimit memlock=82000:82000 mycdn sh -c "ulimit -a"
 Test to proxy http://cdn.fititnt.org/img/emerson-rocha-luiz.jpg
 
     curl http://127.0.0.1:8888/http://cdn.fititnt.org/img/emerson-rocha-luiz.jpg
@@ -21,3 +22,19 @@ Test to proxy http://cdn.fititnt.org/img/emerson-rocha-luiz.jpg
 - Use default configurations for nodeapp, but replace if user place a custom
 - Use default configurations for varnish, but replace if user place a custom
 - Use default configurations for nginx, but replace if user place a custom
+
+## Troubleshot
+
+"Running S20-varnish.sh...
+/etc/init.d/varnish: line 36: ulimit: max locked memory: cannot modify limit: Operation not permitted"
+
+Do on machine that runs docker
+
+    sudo su
+    ulimit -l 120000
+
+    # Ubuntu 14.04
+    # vim /etc/init/docker.conf
+    # Centos
+    # vim /etc/sysconfig/docker
+    limit memlock 120000 120000
--- src/index.js
+++ src/index.js
@@ -1,15 +1,70 @@
+/**
+ * Main file. Run with command `node index.js` or similar
+ *
+ * @package       reverse-proxy-cdn
+ * @copyright     Copyright (C) 2016 Alligo Tecnologia Ltda.
+ * @author        Emerson Rocha Luiz <emerson at alligo.com.br>
+ */
 
-var http = require('http');
+var http = require('http'), Request = require('request');
+var PORT = 8888;
 
 
-var PORT=8888; 
-
-var http = require('http'),
-    request = require('request');
-
-http.createServer(function (req, res) {
+/**
+ * 
+ * @param   {Event/Request}  req
+ * @param   {Event/Response} res
+ * @returns void
+ */
+function forEachRequest(req, res) {
   // http://somewhere.com/noo.bin
   //var remoteUrl = domainexample + req.url;
-  var remoteUrl = req.url.substr(1); // remove primeira /
-  request(remoteUrl).pipe(res);
-}).listen(PORT);
+  //var remoteUrl = req.url.substr(1); // remove primeira /
+  //Request(remoteUrl).pipe(res);
+  var remoteUrl = getValidURL(req.url); // remove primeira /
+  if (remoteUrl) {
+    returnObject(remoteUrl, res);
+  } else {
+    returnInvalidObject(remoteUrl, res);
+  }
+}
+
+/**
+ * Return a valid remote url if is valid, and false if fail
+ *
+ * @param   {String}       url  URL
+ * @returns {String|FALSE}
+ */
+function getValidURL(url) {
+  var result = url && url.substr ? url.substr(1) : false;
+  console.log('getValidURL ' + result);
+  if (result.indexOf('http') !== 0) {
+    result = false;
+  }
+
+  return result;
+}
+/**
+ * Handle a invalid request
+ *
+ * @param   {Strong}         remoteUrl  remote url
+ * @param   {Event/Response} res
+ * @returns {void}
+ */
+function returnInvalidObject(remoteUrl, res) {
+  console.log("INFO: invalid request " + remoteUrl);
+}
+
+/**
+ * Handle a valid request
+ *
+ * @param   {Strong}         remoteUrl  remote url
+ * @param   {Event/Response} res
+ * @returns {void}
+ */
+function returnObject(remoteUrl, res) {
+  console.log("DEBUG: requested " + remoteUrl);
+  Request(remoteUrl).pipe(res);
+}
+
+http.createServer(forEachRequest).listen(PORT);
